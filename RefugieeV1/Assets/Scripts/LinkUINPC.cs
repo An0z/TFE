7a5095af4827aba7050ac26602a083376930d72b
An0z_work\An0z_workstation <An0z@hotmail.fr>
2017-03-14 17:32:13 +0100

    rework animator OK Add icon interaction

---
+++ RefugieeV1/Assets/Scripts/LinkUINPC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkUINPC : MonoBehaviour
+{
+    [SerializeField]
+    float Y;
+
+    [SerializeField]
+    Transform Icon;
+
+    [SerializeField]
+    Transform NPC;
+
+    [SerializeField]
+    myCharacter Player;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Icon.position = new Vector3(NPC.position.x, NPC.position.y + Y, NPC.position.z);
+	}
+}
---
+++ RefugieeV1/Assets/Scripts/SeeThrough.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeThrough : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_camera;
+
+    GameObject invisible;
+
+    public Color White;
+        public Color Transparent;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+        RaycastHit[] hits;
+
+        hits = Physics.RaycastAll(m_camera.position, transform.position, 100f);
+        if (hits.Length > 0)
+        {
+            Debug.DrawLine(m_camera.position, transform.position);
+            foreach (RaycastHit hit in hits)
+                if (hit.collider.gameObject.tag == "tohide")
+                {
+                    Debug.Log("There is something in front of the object!:" + hit.collider.name);
+                    if (invisible != hit.collider.gameObject)
+                    {
+                        setInvisible(255f);
+                        invisible = hit.collider.gameObject;
+                        setInvisible(127.5f);
+                    }                   
+                }
+        }
+        else
+        {
+            setInvisible(255f);
+            Debug.Log("Nothing in front");
+        }
+    }
+
+    public void setInvisible(float alpha)
+    {
+        //invisible.GetComponent<Renderer>().material = new Material(invisible.GetComponent<Renderer>().material);
+        //invisible.GetComponent<Renderer>().material.color = new Color(White.r, White.g, White.b, alpha);
+        if(alpha == 127.5)
+        invisible.GetComponent<Renderer>().material.color = White;
+
+        if(alpha == 255)
+        invisible.GetComponent<Renderer>().material.color = Transparent;
+    }
+
+
+}
